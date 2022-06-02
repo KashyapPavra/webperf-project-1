55b9830da9032b40ae6b95bcb089ab4c31b8fcfd
Kashyap Pavra <pavrakashyap@gmail.com>
2022-06-02 12:12:54 +0530

    Assignment final complete

--- server.mjs
+++ server.mjs
@@ -1,10 +1,28 @@
-import { createServer } from 'http'
-import staticHandler from './staticHandler.mjs'
+import { createServer } from "http";
+import staticHandler from "./staticHandler.mjs";
+import express from "express";
+import conpress from "compression";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const server = createServer((req, res) => {
-  staticHandler(req, res)
-})
+const app = express();
+
+app.use(conpress());
+app.use(staticHandler);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticPath = __dirname + "/dist/css";
+
+app.use(
+	express.static(staticPath, {
+		etag: true,
+		lastModified: true,
+	})
+);
+
+const server = createServer(app);
 
 server.listen(4000, () => {
-  console.log('Server running at http://localhost:4000/')
-})
+	console.log("Server running at http://localhost:4000/");
+});
